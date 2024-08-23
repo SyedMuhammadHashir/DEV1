c08c6567a0d347860779d7d2dc254105c57800b3
SyedMuhammadHashir <122075818+SyedMuhammadHashir@users.noreply.github.com>
2024-08-23 23:55:46 +0500

    Add files via upload

---
+++ DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigtor'>
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+         <div className="descriptionbox-description">
+            <p>Step into the world of fashion with our online clothing store, where style meets simplicity. 
+                   Explore a curated collection of trendy apparel for every occasion, meticulously designed to enhance your wardrobe. 
+                    Enjoy a seamless shopping experience with user-friendly navigation, secure transactions, and prompt deliveries. 
+                  Uncover the latest fashion trends, from casual wear to elegant ensembles, all from the comfort of your home. 
+                   Elevate your style effortlessly with our online clothing destination  your go-to source for fashion-forward choices.
+            </p>
+            <p>Engage with our vibrant online community, where customer reviews and ratings provide valuable 
+                insights to help you make informed decisions. Stay tuned for exclusive deals, promotions, and 
+                seasonal sales, making your shopping experience even more delightful.
+            </p>
+         </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
